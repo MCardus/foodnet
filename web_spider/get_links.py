c9836e287f45ac48a3da7529d82f40de1644a570
Ubuntu <ubuntu@ip-172-31-45-250.eu-west-1.compute.internal>
2017-06-06 00:04:56 +0000

    try-except when parsing implemented

--- web_spider/get_links.py
+++ web_spider/get_links.py
@@ -23,25 +23,28 @@ def get_links(mongodb):
 
     # Childs generation
     for parent in list_parents:
-        list_childs = list()
-        page = requests.get(parent)
-        tree = html.fromstring(page.content)
-        node = tree.xpath(xpath_selector)
-        output = "Found "+str(len(node))+" recipe links from parent "+parent+"\n"
-        tmp_count=0
-        for recipe_link in node:
-            tmp_count+=1
-            current_href=host+recipe_link.get('href')
-            list_childs.append(current_href)
-            output+=str(tmp_count)+","+current_href+"\n"
-        logging.debug(output)
+        try:
+            list_childs = list()
+            page = requests.get(parent)
+            tree = html.fromstring(page.content)
+            node = tree.xpath(xpath_selector)
+            output = "Found "+str(len(node))+" recipe links from parent "+parent+"\n"
+            tmp_count=0
+            for recipe_link in node:
+                tmp_count+=1
+                current_href=host+recipe_link.get('href')
+                list_childs.append(current_href)
+                output+=str(tmp_count)+","+current_href+"\n"
+            logging.debug(output)
 
-        # Writing results in mongodb
-        result = dict()
-        result['Web_links']=str(list_childs)
-        result['Web_parent']=parent
-        result['Num_links']=str(len(node))
-        mongodb.save(result,"recipes_links")
+            # Writing results in mongodb
+            result = dict()
+            result['Web_links']=str(list_childs)
+            result['Web_parent']=parent
+            result['Num_links']=str(len(node))
+            mongodb.save(result,"recipes_links")
+	except:
+	    logging.exception("ERROR with parent: "+parent)
 
 
 if __name__ == "__main__": 
